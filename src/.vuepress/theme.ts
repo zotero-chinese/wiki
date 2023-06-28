5c6e1401a8e930b81dae218acb208f3181db1d0e
Northword <northword@outlook.com>
2023-06-28 22:00:30 +0800

    update deps

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -3,153 +3,154 @@ import { zhNavbar } from "./navbar";
 import { zhSidebar } from "./sidebar";
 
 export default hopeTheme({
-    // hostname: "https://vuepress-theme-hope-docs-demo.netlify.app",
+  // hostname: "https://vuepress-theme-hope-docs-demo.netlify.app",
 
-    author: {
-        name: "Zotero Chinese",
-        url: "https://zotero-cn.github.io",
+  author: {
+    name: "Zotero Chinese",
+    url: "https://zotero-cn.github.io",
+  },
+
+  iconAssets: "iconfont",
+
+  logo: "/logo.png",
+
+  repo: "northword/zotero-guide-zh",
+
+  docsDir: "src",
+
+  hotReload: true,
+  sidebarSorter: ["readme", "order", "filename", "title"],
+
+  locales: {
+    "/": {
+      // navbar
+      navbar: zhNavbar,
+
+      // sidebar
+      sidebar: zhSidebar,
+
+      footer: "Zotero 中文小组 | 查看贡献指南",
+
+      displayFooter: true,
+
+      // page meta
+      metaLocales: {
+        editLink: "在 GitHub 上编辑此页",
+      },
+    },
+  },
+
+  // encrypt: {
+  //   config: {
+  //     "/demo/encrypt.html": ["1234"],
+  //     "/zh/demo/encrypt.html": ["1234"],
+  //   },
+  // },
+
+  plugins: {
+    comment: {
+      // @ts-expect-error: You should generate and use your own comment service
+      provider: "Waline",
     },
 
-    iconAssets: "iconfont",
-
-    logo: "/logo.png",
-
-    repo: "northword/zotero-guide-zh",
-
-    docsDir: "src",
-
-    hotReload: true,
-
-    locales: {
-        "/": {
-            // navbar
-            navbar: zhNavbar,
-
-            // sidebar
-            sidebar: zhSidebar,
-
-            footer: "Zotero 中文小组 | 查看贡献指南",
-
-            displayFooter: true,
-
-            // page meta
-            metaLocales: {
-                editLink: "在 GitHub 上编辑此页",
-            },
+    // all features are enabled for demo, only preserve features you need here
+    mdEnhance: {
+      align: true,
+      attrs: true,
+      chart: true,
+      codetabs: true,
+      // demo: true,
+      // echarts: true,
+      figure: true,
+      flowchart: true,
+      gfm: true,
+      imgLazyload: true,
+      imgSize: true,
+      include: true,
+      katex: true,
+      mark: true,
+      mermaid: true,
+      // playground: {
+      //   presets: ["ts", "vue"],
+      // },
+      // presentation: {
+      //     plugins: ["highlight", "math", "search", "notes", "zoom"],
+      // },
+      stylize: [
+        {
+          matcher: "Recommended",
+          replacer: ({ tag }) => {
+            if (tag === "em")
+              return {
+                tag: "Badge",
+                attrs: { type: "tip" },
+                content: "Recommended",
+              };
+          },
         },
+      ],
+      sub: true,
+      sup: true,
+      tabs: true,
+      // vPre: true,
+      // vuePlayground: true,
     },
 
-    // encrypt: {
-    //   config: {
-    //     "/demo/encrypt.html": ["1234"],
-    //     "/zh/demo/encrypt.html": ["1234"],
+    // uncomment these if you want a pwa
+    // pwa: {
+    //   favicon: "/favicon.ico",
+    //   cacheHTML: true,
+    //   cachePic: true,
+    //   appendBase: true,
+    //   apple: {
+    //     icon: "/assets/icon/apple-icon-152.png",
+    //     statusBarColor: "black",
+    //   },
+    //   msTile: {
+    //     image: "/assets/icon/ms-icon-144.png",
+    //     color: "#ffffff",
+    //   },
+    //   manifest: {
+    //     icons: [
+    //       {
+    //         src: "/assets/icon/chrome-mask-512.png",
+    //         sizes: "512x512",
+    //         purpose: "maskable",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-mask-192.png",
+    //         sizes: "192x192",
+    //         purpose: "maskable",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-192.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //       },
+    //     ],
+    //     shortcuts: [
+    //       {
+    //         name: "Demo",
+    //         short_name: "Demo",
+    //         url: "/demo/",
+    //         icons: [
+    //           {
+    //             src: "/assets/icon/guide-maskable.png",
+    //             sizes: "192x192",
+    //             purpose: "maskable",
+    //             type: "image/png",
+    //           },
+    //         ],
+    //       },
+    //     ],
     //   },
     // },
-
-    plugins: {
-        comment: {
-            // @ts-expect-error: You should generate and use your own comment service
-            provider: "Waline",
-        },
-
-        // all features are enabled for demo, only preserve features you need here
-        mdEnhance: {
-            align: true,
-            attrs: true,
-            chart: true,
-            codetabs: true,
-            // demo: true,
-            // echarts: true,
-            figure: true,
-            flowchart: true,
-            gfm: true,
-            imgLazyload: true,
-            imgSize: true,
-            include: true,
-            katex: true,
-            mark: true,
-            mermaid: true,
-            // playground: {
-            //   presets: ["ts", "vue"],
-            // },
-            // presentation: {
-            //     plugins: ["highlight", "math", "search", "notes", "zoom"],
-            // },
-            stylize: [
-                {
-                    matcher: "Recommended",
-                    replacer: ({ tag }) => {
-                        if (tag === "em")
-                            return {
-                                tag: "Badge",
-                                attrs: { type: "tip" },
-                                content: "Recommended",
-                            };
-                    },
-                },
-            ],
-            sub: true,
-            sup: true,
-            tabs: true,
-            // vPre: true,
-            // vuePlayground: true,
-        },
-
-        // uncomment these if you want a pwa
-        // pwa: {
-        //   favicon: "/favicon.ico",
-        //   cacheHTML: true,
-        //   cachePic: true,
-        //   appendBase: true,
-        //   apple: {
-        //     icon: "/assets/icon/apple-icon-152.png",
-        //     statusBarColor: "black",
-        //   },
-        //   msTile: {
-        //     image: "/assets/icon/ms-icon-144.png",
-        //     color: "#ffffff",
-        //   },
-        //   manifest: {
-        //     icons: [
-        //       {
-        //         src: "/assets/icon/chrome-mask-512.png",
-        //         sizes: "512x512",
-        //         purpose: "maskable",
-        //         type: "image/png",
-        //       },
-        //       {
-        //         src: "/assets/icon/chrome-mask-192.png",
-        //         sizes: "192x192",
-        //         purpose: "maskable",
-        //         type: "image/png",
-        //       },
-        //       {
-        //         src: "/assets/icon/chrome-512.png",
-        //         sizes: "512x512",
-        //         type: "image/png",
-        //       },
-        //       {
-        //         src: "/assets/icon/chrome-192.png",
-        //         sizes: "192x192",
-        //         type: "image/png",
-        //       },
-        //     ],
-        //     shortcuts: [
-        //       {
-        //         name: "Demo",
-        //         short_name: "Demo",
-        //         url: "/demo/",
-        //         icons: [
-        //           {
-        //             src: "/assets/icon/guide-maskable.png",
-        //             sizes: "192x192",
-        //             purpose: "maskable",
-        //             type: "image/png",
-        //           },
-        //         ],
-        //       },
-        //     ],
-        //   },
-        // },
-    },
+  },
 });
