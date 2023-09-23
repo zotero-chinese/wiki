5fbd629059edefeb51713ce7ea60ad7e6ca4e9d4
Northword <northword@outlook.com>
2023-09-23 17:46:12 +0800

    修复百度统计不生效的问题

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,7 +1,7 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 import { searchProPlugin } from "vuepress-plugin-search-pro";
-import { baiduTongjiPlugin } from "@vuepress-plume/vuepress-plugin-baidu-tongji";
+// import { baiduTongjiPlugin } from "@vuepress-plume/vuepress-plugin-baidu-tongji";
 
 export default defineUserConfig({
   base: "/",
@@ -22,10 +22,38 @@ export default defineUserConfig({
       // 为分类和标签添加索引
     }),
 
-    /** @see https://www.npmjs.com/package/@vuepress-plume/vuepress-plugin-baidu-tongji */
-    baiduTongjiPlugin({
-      key: process.env.CONTEXT === "production" ? "fc5b45ae006a231c1d5cff4610df7267" : "", // 百度统计使用的 key
-    }),
+    /**
+     * 百度统计
+     * @see https://www.npmjs.com/package/@vuepress-plume/vuepress-plugin-baidu-tongji
+     *
+     */
+    // baiduTongjiPlugin({
+    //   key: "fc5b45ae006a231c1d5cff4610df7267",
+    //   // key: process.env.CONTEXT === "production" ? "fc5b45ae006a231c1d5cff4610df7267" : "", // 百度统计使用的 key
+    // }),
+    () => {
+      const key =
+        process.env.CONTEXT === "production"
+          ? "fc5b45ae006a231c1d5cff4610df7267"
+          : "";
+      return {
+        name: "vuepress-plugin-baidu-tongji",
+        extendsPage: (page) => {
+          page.frontmatter.head = page.frontmatter.head || [];
+          page.frontmatter.head?.push([
+            "script",
+            {
+              type: "text/javascript",
+            },
+            "var _hmt = _hmt || []",
+          ]);
+          page.frontmatter.head?.push([
+            "script",
+            { src: `https://hm.baidu.com/hm.js?${key}` },
+          ]);
+        },
+      };
+    },
   ],
 
   // Enable it with pwa
