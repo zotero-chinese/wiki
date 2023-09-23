5642e394afa55a01bdc13b85f3771c81b66fa386
Northword <44738481+northword@users.noreply.github.com>
2023-09-23 16:17:05 +0800

    增加百度统计，更新依赖 (#13)
    * 添加百度统计
    
    * Update deps
    
    * 删除 npm 锁定文件
    
    * 移除环境变量，环境变量需要在netlify添加
    
    * 添加百度统计Key
    
    * 非生产环境不引入百度统计

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,27 +1,33 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 import { searchProPlugin } from "vuepress-plugin-search-pro";
+import { baiduTongjiPlugin } from "@vuepress-plume/vuepress-plugin-baidu-tongji";
 
 export default defineUserConfig({
-    base: "/",
+  base: "/",
 
-    locales: {
-        "/": {
-            lang: "zh-CN",
-            title: "Zotero 中文小组",
-            description: "Zotero 非官方中文维护小组",
-        },
+  locales: {
+    "/": {
+      lang: "zh-CN",
+      title: "Zotero 中文小组",
+      description: "Zotero 非官方中文维护小组",
     },
+  },
 
-    theme,
-    plugins: [
-        searchProPlugin({
-            // 索引全部内容
-            indexContent: true,
-            // 为分类和标签添加索引
-        }),
-    ],
+  theme,
+  plugins: [
+    searchProPlugin({
+      // 索引全部内容
+      indexContent: true,
+      // 为分类和标签添加索引
+    }),
 
-    // Enable it with pwa
-    // shouldPrefetch: false,
+    /** @see https://www.npmjs.com/package/@vuepress-plume/vuepress-plugin-baidu-tongji */
+    baiduTongjiPlugin({
+      key: process.env.CONTEXT === "production" ? "fc5b45ae006a231c1d5cff4610df7267" : "", // 百度统计使用的 key
+    }),
+  ],
+
+  // Enable it with pwa
+  // shouldPrefetch: false,
 });
