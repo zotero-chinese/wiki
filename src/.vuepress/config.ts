ddc6fe6159abeac2138cc728b2a1146373843336
Northword <44738481+northword@users.noreply.github.com>
2024-03-07 19:16:18 +0800

    chore: use office baidu plugin (#141)

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,6 +2,7 @@ import { defineUserConfig } from "vuepress";
 import { viteBundler } from "@vuepress/bundler-vite";
 import theme from "./theme.js";
 import { baiduTongjiPlugin } from "./plugins/baiduTongji.js";
+import { baiduAnalyticsPlugin } from "@vuepress/plugin-baidu-analytics";
 
 export default defineUserConfig({
   base: "/",
@@ -28,9 +29,12 @@ export default defineUserConfig({
         process.env.CONTEXT === "production"
           ? [
               "fc5b45ae006a231c1d5cff4610df7267", // lin
-              "fc904ee59939c7c2284063619b0ef533", // northword
+              // "fc904ee59939c7c2284063619b0ef533", // northword
             ]
           : [],
+    }),
+    baiduAnalyticsPlugin({
+      id: "fc904ee59939c7c2284063619b0ef533", // northword
     }),
   ],
 
